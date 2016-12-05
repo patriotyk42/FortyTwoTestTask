f9b56ac325ee9b2bc257e7190f90eecbc10083b7
Serhiy Stetskovych <patriotyk@gmail.com>
2016-12-05 14:23:44 +0000

    ticket:1 added model

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    date_of_birth = models.DateField(null=True)
+    bio = models.TextField(null=True)
+    email = models.EmailField(null=True)
+    jabber = models.EmailField(null=True)
+    skype = models.CharField(max_length=200, null=True)
+    other_contacts = models.TextField(null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.surname)
